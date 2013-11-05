4a48444c2784d059b71b765b72bca9d24b26b6d7
sushramesh <sushanth@live.in>
2013-11-05 10:29:01 +0000

    Changed web.js to start showing index.html instead of Hello World

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  //response.send('Hello World!');
+    response.send(fs.readFileSync("./index.html").toString()); 
 });
 
 var port = process.env.PORT || 8080;
