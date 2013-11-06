cccd1236d5722badcacd35aa309ca7d767c4bb5b
sushramesh <sushanth@live.in>
2013-11-06 13:26:07 +0000

    Trying an updated web.js with updated buffer and file reading

--- web.js
+++ web.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
-var data = fs.readFileSync("index.html","utf-8");
+//var data = fs.readFileSync("index.html","utf-8");
+
+//var buffer = new Buffer(fs.readFile('index.html','utf8'));
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World!');
-    response.send(data); 
+    response.send(fs.readFileSync('index.html','utf8')); 
 });
 
 var port = process.env.PORT || 8080;
